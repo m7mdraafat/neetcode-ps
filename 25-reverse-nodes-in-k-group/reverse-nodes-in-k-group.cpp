f320945553e6f01a4a22201f20b358f16efaf439
Mohamed Raafat <141199348+m7mdraafat@users.noreply.github.com>
2026-04-12 13:27:11 +0200

    Time: 0 ms (100.00%) | Memory: 16.6 MB (38.46%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -10,17 +10,18 @@
  */
 class Solution {
 public:
+    // O(n) time, O(n/k) recursion stack depth
     ListNode* reverseKGroup(ListNode* head, int k) {
         ListNode* cursor = head;
         for (int i = 0; i < k; i++) {
-            if (cursor == nullptr) return head;
+            if (cursor == nullptr) return head; // fewer thank k nodes left, don't reverse (base case)
             cursor = cursor->next;
         }
 
         // cursor now is k + 1 node (first node of the second group)
-        ListNode* curr = head;
-        ListNode* prev = nullptr;
-        ListNode* nxt = nullptr;
+        ListNode* curr = head; // first node of the next group
+        ListNode* prev = nullptr; // new head of reversed group
+        ListNode* nxt = nullptr; 
 
         // reverse current group
         for (int i = 0; i < k; i++) {
@@ -30,8 +31,7 @@ public:
             curr = nxt;
         }
 
-        // link tail of current group to the new head of next group
-        head->next = reverseKGroup(curr, k);
+        head->next = reverseKGroup(curr, k); // head is now the tail of reversed group map to the new head of next group.
         return prev;
     }
 };
